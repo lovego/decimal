c19cab4bd995916959b0879d24461c6c76b471b0
廖恒晨 <liaohengchen@gmail.com>
2021-09-16 15:54:44 +0800

    fractional.go rename to bits.go; add IntBits

--- bits.go
+++ bits.go
@@ -2,15 +2,32 @@ package decimal
 
 import "math/big"
 
+func (d Decimal) IntBits() uint {
+	var value = d.IntPart()
+	if value < 0 {
+		value = -value
+	}
+	var bits uint = 1
+	for {
+		value = value / 10
+		if value > 0 {
+			bits++
+		} else {
+			break
+		}
+	}
+	return bits
+}
+
 // FractionalBits returns the number of significant fractional digits.
 func (d Decimal) FractionalBits() uint {
 	if d.Equal(Zero) || d.exp >= 0 {
 		return 0
 	}
-	exp := uint(-d.exp)
+	exponent := uint(-d.exp)
 	zeroBits := TrailingZeroBits(d.value) // only go1.14+ big.Int have TrailingZeroBits method
-	if exp > zeroBits {
-		return exp - zeroBits
+	if exponent > zeroBits {
+		return exponent - zeroBits
 	}
 	return 0
 }
@@ -18,6 +35,7 @@ func (d Decimal) FractionalBits() uint {
 var intTen = big.NewInt(10)
 var intZero = big.NewInt(0)
 
+// how many zero at end of value
 func TrailingZeroBits(value *big.Int) uint {
 	var quotient, remaider = big.NewInt(0).Set(value), big.NewInt(0)
 	var bits uint
--- bits_test.go
+++ bits_test.go
@@ -2,6 +2,26 @@ package decimal
 
 import "fmt"
 
+func ExampleDecimal_IntBits() {
+	fmt.Println(New(1, 0).IntBits())
+	fmt.Println(New(12, 0).IntBits())
+	fmt.Println(New(1, 1).IntBits())
+	fmt.Println(New(100, 0).IntBits())
+	fmt.Println(New(100, -1).IntBits())
+	fmt.Println(New(100, -2).IntBits())
+	fmt.Println(New(100, 1).IntBits())
+	fmt.Println(New(0, -2).IntBits())
+	// Output:
+	// 1
+	// 2
+	// 2
+	// 3
+	// 2
+	// 1
+	// 4
+	// 1
+}
+
 func ExampleDecimal_FractionalBits() {
 	fmt.Println(New(1, 0).FractionalBits())
 	fmt.Println(New(12, 0).FractionalBits())
